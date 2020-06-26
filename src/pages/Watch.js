cb45af2b0d5b15a9aad156334f4ac22bf45e48f1
tune <hisatsune01@gmail.com>
2020-06-26 15:35:17 +0900

    add SideListItem

--- src/pages/Watch.js
+++ src/pages/Watch.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
 import VideoDetail from '../components/VideoDetail/VideoDetail';
+import SideList from '../components/SideList/SideList';
 
 const Watch = () => {
   return (
     <div>
       <Layout>
         <VideoDetail />
+        <SideList />
       </Layout>
     </div>
   );
